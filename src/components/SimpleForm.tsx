9f908798bd51f24847d8f6696555ac6373db3e6f
happyeveryone96 <happyeveryone@naver.com>
2023-03-01 13:14:30 +0900

    feat: 에러가 있을 때 제출 버튼을 클릭하면 제출되지 않는 기능 구현

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -9,15 +9,25 @@ export const FormContext = createContext({
 
 const SimpleForm = ({ children }: PropsWithChildren<{}>) => {
   const [values, setValues] = useState({ name: "", password: "" });
-  const [errors, setErrors] = useState({ name: "", password: "" });
+  const [errors, setErrors] = useState({
+    name: undefined,
+    password: undefined,
+  });
   const value = useMemo(
     () => ({ setValues, values, errors, setErrors }),
     [setValues, values, errors, setErrors]
   );
 
+  const isError = errors.name !== undefined || errors.password !== undefined;
+  const isEmpty = values.name === "" && values.password === "";
+
   const onClick = (e: any) => {
     e.preventDefault();
-    alert(JSON.stringify(values));
+    if (isError || isEmpty) {
+      alert("제출에 실패하였습니다. 회원 정보를 확인해주세요.");
+    } else {
+      alert(JSON.stringify(values));
+    }
   };
 
   return (
