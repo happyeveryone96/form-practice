185986bce4c7083c03ece75fa27ae2b5c1ee4928
happyeveryone96 <happyeveryone@naver.com>
2023-03-12 00:52:29 +0900

    test: 테스트 코드 추가

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,21 +1,79 @@
 /**
  * @jest-environment jsdom
  */
-import {render} from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import SimpleForm from "../src/components/SimpleForm";
 import TextField from "../src/components/TextField";
+import SelectboxField from "../src/components/SelectboxField";
+import CheckboxField from "../src/components/CheckboxField";
+import { max, min } from "../src/utils/validate";
 
-describe('', () => {
-    it('should render TextField', () => {
-        const input = render(
-            <SimpleForm>
-                <TextField
-                    source={'name'}
-                    label={'name'}
-                />
-            </SimpleForm>
-        );
+describe("TextField", () => {
+  const { queryByText, getByLabelText } = render(
+    <SimpleForm>
+      <TextField source={"name"} label={"이름"} validate={[min(5), max(10)]} />
+    </SimpleForm>
+  );
+  const name = getByLabelText("이름");
 
-        //expect().toBeInDocument()
-    })
+  it("validates the input length using the min validators", () => {
+    fireEvent.change(name, { target: { value: "x".repeat(4) } });
+    expect(queryByText(/자 이상 입력해주세요./));
+  });
+
+  it("validates the input length using the max validators", () => {
+    fireEvent.change(name, { target: { value: "x".repeat(11) } });
+    expect(queryByText(/자 이하로 입력해주세요./));
+  });
+
+  it("renders TextField component", () => {
+    render(
+      <TextField source={"name"} label={"이름"} validate={[min(5), max(10)]} />
+    );
+
+    expect(screen.getByText("이름")).toBeInTheDocument();
+  });
+
+  it("renders TextField component", () => {
+    render(
+      <TextField
+        source={"password"}
+        label={"비밀번호"}
+        validate={[min(5), max(10)]}
+      />
+    );
+
+    expect(screen.getByText("비밀번호")).toBeInTheDocument();
+  });
+});
+
+describe("SelectboxField", () => {
+  it("renders SelectboxField component", () => {
+    render(
+      <SelectboxField
+        source="color"
+        label="색깔"
+        options={[
+          { key: 1, value: "orange" },
+          { key: 2, value: "red" },
+        ]}
+      />
+    );
+
+    expect(screen.getByText("색깔")).toBeInTheDocument();
+  });
+});
+
+describe("CheckboxField", () => {
+  it("renders CheckboxField component", () => {
+    render(
+      <CheckboxField
+        source="marketingReception"
+        label="마케팅 수신"
+        type="checkbox"
+      />
+    );
+
+    expect(screen.getByText("마케팅 수신")).toBeInTheDocument();
+  });
 });
